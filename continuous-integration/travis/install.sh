f7a459eb81ad024d8d65f267808c51d1f5ff790c
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-05-31 16:36:52 +0200

    travis : explicitely set the miniconda install path (avoid issue with 2/3 suffix)

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -1,3 +1,4 @@
+set -e
 MINICONDA_URL="http://repo.continuum.io/miniconda"
 if [ $TRAVIS_OS_NAME == 'osx' ]; then
   MINICONDA_FILE="Miniconda-latest-MacOSX-x86_64.sh"
@@ -5,9 +6,9 @@ else
   MINICONDA_FILE="Miniconda-latest-Linux-x86_64.sh"
 fi
 wget "${MINICONDA_URL}/${MINICONDA_FILE}"
-bash $MINICONDA_FILE -b
+bash $MINICONDA_FILE -b -p ~/miniconda
 
-export PATH=$HOME/miniconda/bin:$PATH
+export PATH=~/miniconda/bin:$PATH
 
 conda update --yes conda
 conda install --yes pip conda-build jinja2 anaconda-client
