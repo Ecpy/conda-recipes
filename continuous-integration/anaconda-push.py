4a8665b488e4400ba9c3738e911eb47729e4df8c
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-06-01 15:44:26 +0200

    continuous-integration : make globing for uploading more restrictive

--- continuous-integration/anaconda-push.py
+++ continuous-integration/anaconda-push.py
@@ -6,7 +6,9 @@ import traceback
 
 token = os.environ['ANACONDA_TOKEN']
 cmd = ['anaconda', '-t', token, 'upload', '--force']
-packages = glob.glob('*/*.tar.bz2')
+packages = []
+for anchor in ('noarch', 'linux-64', 'osx-64', 'win-32', 'win-64'):
+    packages.extend(glob.glob(anchor + '/*.tar.bz2'))
 print('Found packages are :\n', packages)
 cmd.extend(packages)
 try:
