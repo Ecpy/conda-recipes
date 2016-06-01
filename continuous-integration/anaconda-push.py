eeec397ae5c520d38949c35d91d3b9115316ee28
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-06-01 14:00:37 +0200

    more general glob pattern matching for all subdirectories (so that we found packages in win-32, linux-64, ...)

--- continuous-integration/anaconda-push.py
+++ continuous-integration/anaconda-push.py
@@ -5,7 +5,7 @@ import traceback
 
 token = os.environ['ANACONDA_TOKEN']
 cmd = ['anaconda', '-t', token, 'upload', '--force']
-cmd.extend(glob.glob('*.tar.bz2'))
+cmd.extend(glob.glob('*/*.tar.bz2'))
 try:
     subprocess.check_call(cmd)
 except subprocess.CalledProcessError:
