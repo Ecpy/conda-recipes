37258e37e4e2cfb5098fe36866509d1b2bfc6c07
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-06-01 11:48:59 +0200

    appveyor : only copy (not move) packages so that we have atom and kiwi in local channel
    build :
    - [argh]
    - [pathtools]
    - [watchdog]
    - [atom]
    - [kiwisolver]
    - [enaml]

--- continuous-integration/appveyor/build-packages.bat
+++ continuous-integration/appveyor/build-packages.bat
@@ -9,16 +9,16 @@ REM globbing, so we'll use a simple python script.
 if %BUILD_ATOM%==0 (
   conda build .\\conda-recipes\\atom --quiet'
   conda index %CONDA_BLD_PATH%\win-%PYTHON_ARCH%'
-  python continuous-integration\move-conda-packages.py conda-recipes\atom'
+  python continuous-integration\copy-conda-packages.py conda-recipes\atom'
 )
 
 if %BUILD_KIWISOLVER%==0 (
   conda build .\\conda-recipes\\kiwisolver --quiet
   conda index %CONDA_BLD_PATH%\win-%PYTHON_ARCH%
-  python continuous-integration\move-conda-packages.py conda-recipes\kiwisolver
+  python continuous-integration\copy-conda-packages.py conda-recipes\kiwisolver
 )
 
 if %BUILD_ENAML%==0 (
   conda build .\\conda-recipes\\enaml --quiet -c file://%CONDA_BLD_PATH%
-  python continuous-integration\move-conda-packages.py conda-recipes\enaml
+  python continuous-integration\copy-conda-packages.py conda-recipes\enaml
 )
--- continuous-integration/copy-conda-package.py
+++ continuous-integration/copy-conda-package.py
@@ -11,4 +11,4 @@ with open(os.path.join(sys.argv[1], 'meta.yaml')) as f:
 binary_package_glob = os.path.join(config.bldpkgs_dir, '{0}*.tar.bz2'.format(name))
 binary_package = glob.glob(binary_package_glob)[0]
 
-shutil.move(binary_package, '.')
+shutil.copy(binary_package, '.')
