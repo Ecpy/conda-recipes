b6b651b385e65d4c935c31b324e5a8fb4455a2e2
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-06-01 14:38:47 +0200

    appveyor : try to fix build by splitting the build script in three and set properly the artifacts path.
    build :
    - [argh]
    - [pathtools]
    - [watchdog]
    - [atom]
    - [kiwisolver]
    - [enaml]

---
+++ continuous-integration/appveyor/build-enaml.bat
@@ -0,0 +1,7 @@
+REM Build and test the package. This appears(?) to sporadically fail due to a
+REM bug in conda-build on 32 bit python.
+REM https://github.com/conda/conda-build/issues/152
+
+if %BUILD_ENAML%==0 (
+  conda build .\\conda-recipes\\enaml --quiet -c file://%CONDA_BLD_PATH%
+)
