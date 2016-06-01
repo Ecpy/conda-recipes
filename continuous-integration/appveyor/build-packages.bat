5540d259f7d971f5ebb8102fda0ac7689a40d8cf
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-06-01 09:13:21 +0200

    appveyor : use an external script to build packages (avoid nightmare of multiline commands in yaml
    build :
    - [argh]
    - [pathtools]
    - [watchdog]
    - [atom]
    - [kiwisolver]
    - [enaml]

---
+++ continuous-integration/appveyor/build-packages.bat
@@ -0,0 +1,24 @@
+REM Build and test the package. This appears(?) to sporadically fail due to a
+REM bug in conda-build on 32 bit python.
+REM https://github.com/conda/conda-build/issues/152
+REM
+REM Once built the conda packages are moved into the current directory, to
+REM register them as "artifacts" for Appveyor. cmd.exe does't have good
+REM globbing, so we'll use a simple python script.
+
+if %BUILD_ATOM%==0 (
+  %CMD_IN_ENV% conda build .\\conda-recipes\\atom --quiet'
+  conda index %CONDA_BLD_PATH%\win-%PYTHON_ARCH%'
+  python continuous-integration\move-conda-packages.py conda-recipes\atom'
+)
+
+if %BUILD_KIWISOLVER%==0 (
+  %CMD_IN_ENV% conda build .\\conda-recipes\\kiwisolver --quiet
+  conda index %CONDA_BLD_PATH%\win-%PYTHON_ARCH%
+  python continuous-integration\move-conda-packages.py conda-recipes\kiwisolver
+)
+
+if %BUILD_ENAML%==0 (
+  %CMD_IN_ENV% conda build .\\conda-recipes\\enaml --quiet -c file://%CONDA_BLD_PATH%
+  python continuous-integration\move-conda-packages.py conda-recipes\enaml
+)
