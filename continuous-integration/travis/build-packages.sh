dabca8ca546a33b4890bb43167b273eb270ae53e
MatthieuDartiailh <marul@laposte.net>
2016-11-30 19:29:03 +0100

    Fix build (hopefully) and skip some build on osx - [argh] - [pathtools] - [enaml-pygments] - [kiwisolver] - [pyclibrary] - [pyvisa]

--- continuous-integration/travis/build-packages.sh
+++ continuous-integration/travis/build-packages.sh
@@ -1,42 +1,44 @@
 set -e
 if [[ $DISABLE_TRAVIS != 0 ]]; then
-  if [[ $BUILD_ARGH == 0 ]]; then
-    conda build conda-recipes/argh --quiet
-    export BUILD_ARGH=$?
-    export PACK="$(conda build conda --output)"
-    conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
-    conda index $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64
-  fi
+  if [[ $TRAVIS_OS_NAME == linux ]]; then
+      if [[ $BUILD_ARGH == 0 ]]; then
+        conda build conda-recipes/argh --quiet
+        export BUILD_ARGH=$?
+        export PACK="$(conda build conda-recipes/argh --output)"
+        conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
+        conda index $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64
+      fi
 
-  if [[ $BUILD_PATHTOOLS == 0 ]]; then
-    conda build conda-recipes/pathtools --quiet
-    export BUILD_PATHTOOLS=$?
-    export PACK="$(conda build conda --output)"
-    conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
-    conda index $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64
-  fi
+      if [[ $BUILD_PATHTOOLS == 0 ]]; then
+        conda build conda-recipes/pathtools --quiet
+        export BUILD_PATHTOOLS=$?
+        export PACK="$(conda build conda-recipes/pathtools --output)"
+        conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
+        conda index $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64
+      fi
 
-  if [[ $BUILD_ENAML_PYGMENTS == 0 ]]; then
-    conda build conda-recipes/enaml-pygments --quiet
-    export BUILD_ENAML_PYGMENTS=$?
-    export PACK="$(conda build conda --output)"
-    conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
-    conda index $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64
-  fi
+      if [[ $BUILD_ENAML_PYGMENTS == 0 ]]; then
+        conda build conda-recipes/enaml-pygments --quiet
+        export BUILD_ENAML_PYGMENTS=$?
+        export PACK="$(conda build conda-recipes/enaml-pygments --output)"
+        conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
+        conda index $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64
+      fi
 
-  if [[ $BUILD_PYCLIBRARY == 0 ]]; then
-    conda build conda-recipes/pyclibrary --quiet
-    export BUILD_PYCLIBRARY=$?
-    export PACK="$(conda build conda --output)"
-    conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
-    conda index $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64
-  fi
-  
-  if [[ $BUILD_PYVISA == 0 ]]; then
-    conda build conda-recipes/pyvisa --quiet
-    export BUILD_PYVISA=$?
-    export PACK="$(conda build conda --output)"
-    conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
+      if [[ $BUILD_PYCLIBRARY == 0 ]]; then
+        conda build conda-recipes/pyclibrary --quiet
+        export BUILD_PYCLIBRARY=$?
+        export PACK="$(conda build conda-recipes/pyclibrary --output)"
+        conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
+        conda index $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64
+      fi
+
+      if [[ $BUILD_PYVISA == 0 ]]; then
+        conda build conda-recipes/pyvisa --quiet
+        export BUILD_PYVISA=$?
+        export PACK="$(conda build conda-recipes/pyvisa --output)"
+        conda convert $PACK -p 'all' --quiet -o $CONDA_BLD_PATH
+      fi
   fi
 
   if [[ $BUILD_WATCHDOG == 0 ]]; then
