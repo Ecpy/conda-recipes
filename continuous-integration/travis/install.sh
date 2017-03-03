ccad8ef81e9bd220feea90b7bae0427767678e5b
MatthieuDartiailh <marul@laposte.net>
2017-03-03 13:24:52 +0100

    fix install on Travis
    [skip-appveyor]

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -1,9 +1,15 @@
 set -e
 MINICONDA_URL="http://repo.continuum.io/miniconda"
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  MINICONDA_FILE="Miniconda-latest-MacOSX-x86_64.sh"
+MINICONDA_FILE="Miniconda"
+if [[ $CONDA_PY == '27' ]]; then
+  MINICONDA_FILE+="2"
 else
-  MINICONDA_FILE="Miniconda-latest-Linux-x86_64.sh"
+  MINICONDA_FILE+="3"
+fi
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  MINICONDA_FILE+="-latest-MacOSX-x86_64.sh"
+else
+  MINICONDA_FILE+="-latest-Linux-x86_64.sh"
 fi
 wget "${MINICONDA_URL}/${MINICONDA_FILE}"
 bash $MINICONDA_FILE -b -p ~/miniconda
